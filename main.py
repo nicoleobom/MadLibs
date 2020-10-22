190a90f830f372a6f5562ab9aca3dfaa601350a5
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-10-22 17:49:56 -0400

    adding intro

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+intro = input("Hi! What's your name?")
+
 adjective = input("Enter an adjective: ")
+
 adjective2 = input("Enter another adjective: ")
+
 room = input("Enter the name of a room: ")
+
 plural-noun = input("Enter a plural noun: ")
+
 plural-noun2 = input("Enter another plural noun: ")
+
 noun = input("Gimme a noun!: ")
+
 noun2 = input("Gimme another noun!: ")
+
 noun3 = input("Ok, one more noun: ")
+
 adjective3 = input("Let's change it up. Give me an adjective: ")
+
 noun4 = input("And yet another noun: ")
+
 number = input("Now enter a number: ")
+
 plural-noun3 = input("Enter a plural noun: ")
+
 noun5 = input("Enter another regular noun: ")
+
 color = input("What's your favorite color?: ")
+
 color2 = input("What's your least favorite color?: ")
+
 something-alive = input("Name something in the same room as you that is living: ")
+
 person1 = input("Name a person in the same room or household as you: ")
+
 person2 = input("Name another person in the same room or household as you: ")
 
